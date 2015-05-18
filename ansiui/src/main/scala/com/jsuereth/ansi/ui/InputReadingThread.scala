9eda6c3eb4d67309de61a931b24f3da743347686
Josh Suereth <joshua.suereth@gmail.com>
2015-05-18 09:44:33 -0400

    Fix issue where ctrl + key would cause thread to break completely.

--- ansiui/src/main/scala/com/jsuereth/ansi/ui/InputReadingThread.scala
+++ ansiui/src/main/scala/com/jsuereth/ansi/ui/InputReadingThread.scala
@@ -47,28 +47,27 @@ private[ui] class InputReadingThread(e: MainUILoop, in: java.io.InputStream = Sy
     // Here we set a timeout on the read so we can try to detect an "ESC" press vs some other key.
     nonBlockingInput.read(timeout = 40L) match {
         // Read timeout value, this means an escape was pressed, as there is no follow-on characters.
-      case NonblockingInputStreamWrapper.READ_TIMEOUT => Key(27)
+      case NonblockingInputStreamWrapper.READ_TIMEOUT => EscapeKey()
       case '[' =>
-        // TODO - We should consume until an ANSI terminator, and then check the code.
-        // TODO - We should also have this kind of mapping be done via a "keymap".
+        // TODO -
         readAnsiCode() match {
-          //case "A" => UpKey()
-          //case "B" => DownKey()
-          //case "C" => RightKey()
-          //case "D" => LeftKey()
-          //case "F" => End()
-          //case "H" => Home()
           case CursorPositionExcape(row, col) => CursorPosition(row.toInt,col.toInt)
           case s => EscapeCode(s)
         }
-      // TODO - This means escape was pressed.  Here we don't want to consume the n, we just want to
-        // fire the ESC and then read the n.
       case n if (n >= 64) && (n <= 95) => EscapeCode(s"$n")
       case n =>
-        // Here, we may be able to assume 'alt' was held before typing the character, as it's a known thing linux terminals do.
-        e.fire(DisplayText(s"Expected esacpe code, found char $n"))
-        // TODO - ??? we need to expand our handling of keys.
-        sys.error(s"Expecting escape code, found $n")
+        // TODO - It turns out CTRL/ALT + key => leads to this scenario.  We may want better understanding of this scenario, as we may
+        //        be corrupting our input streams by not reading the correct number of characters.
+        //        It appears that we may be capturing "alt" or "ctl" escape, but not capturing the key that was pressed with the
+        //        CTRL/ALT flag.
+        EscapeCode(readNonAnsiCode())
+    }
+  }
+
+  private def readNonAnsiCode(sb: StringBuilder = new StringBuilder): String = {
+    nonBlockingInput.read.toChar match {
+      case n if (n >= '@') || (n <= '~') => sb.append(n).toString
+      case n => sb.append(n); readNonAnsiCode(sb)
     }
   }
 
